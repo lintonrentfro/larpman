3e4691bc4b3e32ce55036b7c9e2064fb489ce61a
Linton Rentfro <linton@protonmail.ch>
2015-10-11 13:27:00 -0700

    adding dev_tools
    dev_tools to make testing and setup easier

---
+++ client/dev_tools.js
@@ -0,0 +1,52 @@
+dev_tools = {
+	test_users : function() {
+		var test_users = [
+			{
+	    		username : "larp_admin",
+	    		password : "coffee"
+    		},
+			{
+				username : "patrick",
+	    		password : "coffee"
+			},
+			{
+				username : "jonathan",
+	    		password : "coffee"
+			},
+			{
+				username : "brent",
+	    		password : "coffee"
+			},
+			{
+				username : "marina",
+	    		password : "coffee"
+			},
+			{
+				username : "lavar",
+	    		password : "coffee"
+			}
+		];
+		return test_users;
+	},
+	create_test_users : function() {
+		var test_users = dev_tools.test_users();
+		for(var i=0;test_users.length > i;i++) {
+			Accounts.createUser(
+		    	{
+		    		username : test_users[i].username,
+		    		password : test_users[i].password
+	    		}
+			);
+		};
+		Meteor.logout();
+	},
+	test : function(){
+		var test_users = dev_tools.test_users();
+		for(var i=0;test_users.length > i;i++) {
+			console.log(test_users[i]);
+		};
+	}
+};
+// dev_tools.create_admin();
+// dev_tools.create_test_users();
+dev_tools.test();
